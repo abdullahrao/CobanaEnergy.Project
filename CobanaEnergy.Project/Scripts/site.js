57a1710224346af1663884d302095f532bacb502
Abdullah <coban@IT>
2025-06-20 13:04:02 +0100

    revert back to original!!!!

--- CobanaEnergy.Project/Scripts/site.js
+++ CobanaEnergy.Project/Scripts/site.js
@@ -1,5 +1,5 @@
 ﻿// site.js
-// Automatically include anti-forgery token in all ajax requests
+// Automatically include anti-forgery token in all Ajax requests
 $(document).ready(function () {
     var token = $('input[name="__RequestVerificationToken"]').val();
     if (token) {
