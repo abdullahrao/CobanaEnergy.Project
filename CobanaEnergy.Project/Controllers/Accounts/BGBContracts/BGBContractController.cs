649b9023b018db070b44d72f4297ceb6582ba17b
Abdullah <coban@IT>
2025-07-11 12:54:49 +0100

    Pre BGB work!

---
+++ CobanaEnergy.Project/Controllers/Accounts/BGBContracts/BGBContractController.cs
@@ -0,0 +1,18 @@
+﻿using CobanaEnergy.Project.Controllers.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CobanaEnergy.Project.Controllers.Accounts.BGBContracts
+{
+    public class BGBContractController : BaseController
+    {
+        [HttpGet]
+        public ActionResult EditBGBContract(string id)
+        {
+            return View();
+        }
+    }
+}
