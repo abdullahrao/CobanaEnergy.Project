30bcd93e31f42fbd1e0c4bf998a008f888b71328
Muhammad Kamarn <partner.m.kamran@axian-group.com>
2025-08-06 17:08:39 +0500

    #86c4btzgh -migration issue fixed

---
+++ CobanaEnergy.Project/Migrations/202508061207452_Campaign_86c4btzgh_InitialMapping.cs
@@ -0,0 +1,16 @@
+﻿namespace CobanaEnergy.Project.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Campaign_86c4btzgh_InitialMapping : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
