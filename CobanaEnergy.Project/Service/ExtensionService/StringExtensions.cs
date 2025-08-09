0bd9a157788bd6b99aa334371fd2efd16b1752e3
Muhammad Kamarn <partner.m.kamran@axian-group.com>
2025-08-09 23:14:31 +0500

    #86c4y049r BGB Changes added

---
+++ CobanaEnergy.Project/Service/ExtensionService/StringExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CobanaEnergy.Project.Service.ExtensionService
+{
+    public static class StringExtensions
+    {
+        public static string ToTwoDecimal(this string value)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+                return string.Empty; // or return value; if you want to keep original null/empty
+
+            if (decimal.TryParse(value, out var dec))
+                return dec.ToString("F2", System.Globalization.CultureInfo.InvariantCulture);
+
+            return value; // return original if not a valid number
+        }   
+    }
+}
