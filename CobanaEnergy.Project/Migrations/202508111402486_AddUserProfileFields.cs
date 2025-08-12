832e3c43efa3b0b266aa0ed1cdb5c0b8723cfebc
Faryal Khan <faryalkhan2824@gmail.com>
2025-08-12 21:11:00 +0500

    Migration file comitted

---
+++ CobanaEnergy.Project/Migrations/202508111402486_AddUserProfileFields.cs
@@ -0,0 +1,18 @@
+﻿namespace CobanaEnergy.Project.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddUserProfileFields : DbMigration
+    {
+        public override void Up()
+        {
+
+        }
+        
+        public override void Down()
+        {
+           
+        }
+    }
+}
